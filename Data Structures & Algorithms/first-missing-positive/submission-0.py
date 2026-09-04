771e161a7dd6a5f1237686fbede8cbbde771061f
Shubham Yadav <127098606+shubhamdev0010@users.noreply.github.com>
2026-09-04 10:14:24 +0530

    Update: first-missing-positive - submission-0

--- Data Structures & Algorithms/first-missing-positive/submission-0.py
+++ Data Structures & Algorithms/first-missing-positive/submission-0.py
@@ -1,13 +1,12 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        n = len(nums)
-        dic = dict()
+        n=len(nums)
+        dic=dict()
         for num in nums:
             if num > 0:
-                dic[num] = True
-        
+                dic[num]=True
 
-        for i in range(1, n+1):
+        for i in range(1,n+1):
             if i not in dic:
                 return i
         return n+1
